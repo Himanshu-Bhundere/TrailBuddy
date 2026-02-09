ae19811f7c657f6ca29787a5f30f35d58af933f0
Himanshu-Bhundere <himanshubhundere@gmail.com>
2026-02-09 21:45:43 +0530

    Change gpt model

--- backend_api.py
+++ backend_api.py
@@ -222,7 +222,7 @@ RULES:
 """
 
     response = client.chat.completions.create(
-        model="gpt-4o-mini",
+        model="gpt-4",
         messages=[
             {
                 "role": "system",
